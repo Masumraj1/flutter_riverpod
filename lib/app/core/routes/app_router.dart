95295d1a4400c4b075728b2d87e968b73b70247e
Masumraj1 <promotionbd321@gmail.com>
2025-11-25 12:21:44 +0600

    Flutter_Riverpod_Full_Documentation_create

--- lib/app/core/routes/app_router.dart
+++ lib/app/core/routes/app_router.dart
@@ -1,7 +1,5 @@
 import 'package:go_router/go_router.dart';
 
-import '../../features/auth/presentaion/pages/sign_in_page.dart';
-import '../../features/auth/presentaion/pages/sign_up_page.dart';
 import 'route_path.dart';
 
 class AppRouter {
@@ -9,16 +7,16 @@ class AppRouter {
     initialLocation: RoutePath.signIn.addBasePath,
     debugLogDiagnostics: true,
     routes: [
-      GoRoute(
-        name: RoutePath.signIn,
-        path: RoutePath.signIn.addBasePath,
-        builder: (context, state) =>  SignInPage(),
-      ),
-      GoRoute(
-        name: RoutePath.signUp,
-        path: RoutePath.signUp.addBasePath,
-        builder: (context, state) =>  SignUpPage(),
-      ),
+      // GoRoute(
+      //   name: RoutePath.signIn,
+      //   path: RoutePath.signIn.addBasePath,
+      //   builder: (context, state) =>  SignInPage(),
+      // ),
+      // GoRoute(
+      //   name: RoutePath.signUp,
+      //   path: RoutePath.signUp.addBasePath,
+      //   builder: (context, state) =>  SignUpPage(),
+      // ),
 
     ],
   );
--- lib/app/features/auth/domain/repositories/auth_repository.dart
+++
@@ -1,6 +0,0 @@
-import '../entities/user.dart';
-
-abstract class AuthRepository {
-  Future<User> signIn(String email, String password);
-  Future<User> signUp(String email, String password);
-}
